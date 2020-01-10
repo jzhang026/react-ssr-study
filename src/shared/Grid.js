d30a687fafe88651f7c059336b1fc14ae7e20651
Adrian Zhang <adrian.zhang@shopee.com>
2020-01-10 11:17:51 +0800

    style change

--- src/shared/Grid.js
+++ src/shared/Grid.js
@@ -49,7 +49,7 @@ class Grid extends Component {
         <h3>{this.props.match.params.id}</h3>
       <ul style={{display: 'flex', flexWrap: 'wrap'}}>
         {repos.map(({ name, owner, stargazers_count, html_url }) => (
-          <li key={name} style={{margin: 25}}>
+          <li key={name} style={{margin: 25, color: 'black', fontSize: '20px'}}>
             <ul>
               <li><a href={html_url}>{name}</a></li>
               <li>@{owner.login}</li>
